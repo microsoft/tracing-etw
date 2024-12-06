8f07c3a7f060f6226237bd424555b1d0e9d3cde8
Kyle Sabo <robo210@gmail.com>
2024-12-06 02:58:57 -0800

    Fix test on windows

--- src/native/etw.rs
+++ src/native/etw.rs
@@ -7,12 +7,13 @@ use tracelogging_dynamic::EventBuilder;
 use tracing_subscriber::registry::{LookupSpan, SpanRef};
 
 // Items within this .rsdata section will be sorted alphabetically, thus the start is named with "0", the end "9", and each metadata "5".
+// If these statics aren't mut then everything will silently fail to work.
 #[allow(non_upper_case_globals)]
 #[link_section = ".rsdata$zRSETW0"]
-pub(crate) static _start__etw_kw: usize = 0;
+pub(crate) static mut _start__etw_kw: usize = 0;
 #[allow(non_upper_case_globals)]
 #[link_section = ".rsdata$zRSETW9"]
-pub(crate) static _stop__etw_kw: usize = 0;
+pub(crate) static mut _stop__etw_kw: usize = 0;
 
 thread_local! {static EBW: std::cell::RefCell<EventBuilder>  = RefCell::new(EventBuilder::new());}
 
